d5cf54774c9912a2312990a47c3eef675b6c5eda
Nathan Santos <nathansr.nsr@gmail.com>
2024-06-14 17:52:07 -0300

    Search com buscador na API, sem filtro no frontend

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,19 +2,41 @@ import { useState } from 'react';
 import styles from './Search.module.css';
 import { SearchProps } from './Search.types';
 import { SearchIcon } from 'lucide-react';
+import axios from '../../config/axios';
 
-const Search: React.FC<SearchProps> = ({ placeholder = 'Buscar por código, nome ou cor', state }) => {
+const Search: React.FC<SearchProps> = ({ placeholder = 'Buscar por código, modelo ou cor', state }) => {
     const [motorcycles, setMotorcycles] = state;
-    const [filter, setFilter] = useState("");
-    const onChangeFilter = () => {
-        const listFiltered = motorcycles.filter(moto => moto.id.toLocaleLowerCase().includes(filter) || moto.color.toLocaleLowerCase().includes(filter) || moto.model.toLocaleLowerCase().includes(filter))
-        setMotorcycles([...listFiltered])
+    let [filter, setFilter] = useState("");
+
+    const onSearch = async () => {
+        const resp = await axios.get(`/motorcycles?color=${filter}&id=${filter}&model=${filter}`)
+        setMotorcycles([...resp.data])
+        // filter = filter.toLocaleLowerCase();
+        // const listFiltered = motorcycles.filter(moto =>
+        //     moto.color.toLowerCase().includes(filter) ||
+        //     moto.model.toLowerCase().includes(filter) ||
+        //     moto.id.toLowerCase() === filter
+        // );
+        // setMotorcycles([...listFiltered])
     }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            onSearch();
+        }
+    };
 
     return (
         <div className={styles.Search}>
-            <SearchIcon size="1.3rem" />
-            <input type="search" placeholder={placeholder} value={filter} onChange={onChangeFilter} />
+            <SearchIcon size="1.3rem" onClick={onSearch} />
+            <input
+                type="search"
+                placeholder={placeholder}
+                value={filter}
+                onChange={e => setFilter(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
         </div>
     );
 }
